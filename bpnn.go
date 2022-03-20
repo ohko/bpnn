32d4a48089abb2f67e1385cf0552138b4a2a8794
ohko <ohko@qq.com>
2022-03-20 19:09:54 +0800

    xor ok

--- bpnn.go
+++ bpnn.go
@@ -1,11 +1,8 @@
 package bpnn
 
 import (
-	"fmt"
-	"log"
 	"math"
 	"math/rand"
-	"strings"
 )
 
 type BPNN struct {
@@ -42,10 +39,14 @@ func NewBPNN(inputCount, outputCount int, hidden []int, learn, diff float64) (*B
 
 	// 初始化隐藏层
 	tmp := [][]Neure{}
-	for _, i := range hidden {
+	for m, i := range hidden {
 		tmp1 := make([]Neure, i)
 		for k := range tmp1 {
-			tmp1[k].Weight = make([]float64, len(o.Input))
+			if m == 0 {
+				tmp1[k].Weight = make([]float64, len(o.Input))
+			} else {
+				tmp1[k].Weight = make([]float64, hidden[m-1])
+			}
 			for j := 0; j < len(o.Input); j++ {
 				tmp1[k].Weight[j] = randFloat64(0, 1)
 			}
@@ -66,8 +67,65 @@ func NewBPNN(inputCount, outputCount int, hidden []int, learn, diff float64) (*B
 	return o, nil
 }
 
+// 计算模块
+func calcForward(input, output []Neure, last bool) {
+	for idxInput := range output {
+		sum := 0.0
+		for idxOutput := range input {
+			sum += input[idxOutput].Value * output[idxInput].Weight[idxOutput]
+			// log.Printf("%.3f += %.3f * %.3f\n", sum, input[idxOutput].Value, output[idxInput].Weight[idxOutput])
+		}
+		if last {
+			output[idxInput].NewValue = sigmoid(sum)
+			// log.Printf("sigmod(x)=%.3f", output[idxInput].NewValue)
+		} else {
+			output[idxInput].Value = sigmoid(sum)
+			// log.Printf("sigmod(x)=%.3f", output[idxInput].Value)
+		}
+	}
+}
+func calcBackward(learn float64, input, output []Neure) {
+	for idxInput := range input {
+		for idxOutput := range input[idxInput].Weight {
+			output[idxOutput].ErrDiff = 0
+		}
+	}
+	for idxInput := range input {
+		for idxOutput := range input[idxInput].Weight {
+			errDiff := input[idxInput].ErrDiff
+			value := output[idxOutput].Value
+			weight := input[idxInput].Weight[idxOutput]
+			input[idxInput].Weight[idxOutput] = weight - (value * -errDiff * learn)
+			output[idxOutput].ErrDiff += (errDiff * weight) * value * (1 - value)
+			// log.Printf("weight: %.3f - (%.3f * -%.3f * %.3f)\n", weight, value, errDiff, learn)
+			// log.Printf("(%.3f * %.3f) * %.3f * (1 - %.3f)\n", errDiff, weight, value, value)
+			// log.Printf("误差:%.3f 残差和：%.3f 修正：%.3f\n", errDiff, output[idxOutput].ErrDiff, input[idxInput].Weight[idxOutput])
+		}
+	}
+}
+
 // Train 训练
-func (o *BPNN) Train(input, output []float64) bool {
+func (o *BPNN) Train(input, output [][]float64, trainCount int) (float64, int) {
+	min := 0.0
+	for mm := 0; mm < trainCount; mm++ {
+		min = 0.0
+		for i := range input {
+			result := o.train(input[i], output[i])
+			if min == 0 {
+				min = result
+			}
+			if result < min {
+				min = result
+			}
+		}
+		if min < 0.0001 {
+			return min, mm
+		}
+	}
+	return min, trainCount
+}
+
+func (o *BPNN) train(input, output []float64) float64 {
 	// 设置值
 	for k, v := range input {
 		o.Input[k].Value = v
@@ -76,99 +134,70 @@ func (o *BPNN) Train(input, output []float64) bool {
 		o.Output[k].Value = v
 	}
 
-	logs := []string{}
-	for nn := 0; nn < 1000; nn++ {
-		// 计算隐藏层
-		log.Println("计算隐藏层")
-		for idx := range o.Hidden {
-			for idx2 := range o.Hidden[idx] {
-				sum := 0.0
-				for idx3, in := range o.Input {
-					sum += in.Value * o.Hidden[idx][idx2].Weight[idx3]
-					log.Printf("%.2f += %.2f * %.2f\n", sum, in.Value, o.Hidden[idx][idx2].Weight[idx3])
-				}
-				o.Hidden[idx][idx2].Value = sigmoid(sum)
-				log.Printf("sigmod(x)=%.2f", o.Hidden[idx][idx2].Value)
-			}
-		}
+	// for nn := 0; nn < 1000; nn++ {
+	// 计算输入层->隐藏层
+	// log.Println("计算输入层->隐藏层")
+	calcForward(o.Input, o.Hidden[0], false)
 
-		// 计算输出层
-		log.Println("计算输出层")
-		last := len(o.Hidden) - 1
-		for idx := range o.Output {
-			sum := 0.0
-			for idx2, hi := range o.Hidden[last] {
-				sum += hi.Value * o.Output[idx].Weight[idx2]
-				log.Printf("%.2f += %.2f * %.2f\n", sum, hi.Value, o.Output[idx].Weight[idx2])
-			}
-			o.Output[idx].NewValue = sigmoid(sum)
-			log.Printf("sigmod(x)=%.2f", o.Output[idx].NewValue)
+	// 计算隐藏层->隐藏层
+	// log.Println("计算隐藏层->隐藏层")
+	last := len(o.Hidden) - 1
+	if last >= 1 {
+		for idx := range o.Hidden[0:last] {
+			calcForward(o.Hidden[idx], o.Hidden[idx+1], false)
 		}
-
-		// 计算误差
-		log.Println("计算误差")
-		minDiff := 0.0
-		for idx := range o.Output {
-			o.Output[idx].Diff = math.Pow(o.Output[idx].NewValue-o.Output[idx].Value, 2)
-			if minDiff == 0.0 {
-				minDiff = o.Output[idx].Diff
-			}
-			if o.Output[idx].Diff < minDiff {
-				minDiff = o.Output[idx].Diff
-			}
-		}
-		log.Printf("diff=%.2f", minDiff)
-
-		// 阀值判断
-		if minDiff < o.Diff {
-			fmt.Println(strings.Join(logs, "\n"))
-			return true
-		}
-
-		// 反向计算残差值
-		log.Println("反向计算残差值")
-		for idx := range o.Output {
-			new, old := o.Output[idx].NewValue, o.Output[idx].Value
-			o.Output[idx].ErrDiff = -(new - old) * new * (1 - new)
-			log.Printf("errDiff=%.2f", o.Output[idx].ErrDiff)
-		}
-
-		// 计算输入层的误差
-		// 输出 -> 最后一个隐藏层
-		log.Println("输出 -> 最后一个隐藏层")
-		for idx2 := range o.Hidden[last] {
-			for idx3 := range o.Output {
-				errDiff := o.Output[idx3].ErrDiff
-				value := o.Hidden[last][idx2].Value
-				weight := o.Output[idx3].Weight[idx2]
-				o.Hidden[last][idx2].ErrDiff = (errDiff * weight) * value * (1 - value)
-				o.Output[idx3].Weight[idx2] = weight - (value * -errDiff * o.Learn)
-				log.Printf("误差:%.3f 修正：%.3f", o.Hidden[last][idx2].ErrDiff, o.Output[idx3].Weight[idx3])
-			}
-		}
-		// 隐藏 -> 输入层
-		for idx := range o.Hidden[0] {
-			for idx2 := range o.Hidden[0][idx].Weight {
-				errDiff := o.Hidden[0][idx].ErrDiff
-				value := o.Input[idx2].Value
-				weight := o.Hidden[0][idx].Weight[idx2]
-				o.Hidden[0][idx].Weight[idx2] = weight - (value * -errDiff * o.Learn)
-				log.Printf("误差:%.3f 修正：%.3f", errDiff, o.Hidden[0][idx2].Weight[idx])
-			}
-		}
-
-		fmt.Printf("1->A:%.3f 2->A:%.3f\n1->B:%.3f 2->B:%.3f\nA->C:%.3f B->C:%.3f\n",
-			o.Hidden[0][0].Weight[0],
-			o.Hidden[0][0].Weight[1],
-			o.Hidden[0][1].Weight[0],
-			o.Hidden[0][1].Weight[1],
-			o.Output[0].Weight[0],
-			o.Output[0].Weight[1])
-		logs = append(logs, fmt.Sprintf("%.8f", minDiff))
 	}
 
-	fmt.Println(strings.Join(logs, "\n"))
-	return false
+	// 计算隐藏层->输出层
+	// log.Println("计算隐藏层->输出层")
+	calcForward(o.Hidden[last], o.Output, true)
+
+	// 计算误差
+	// log.Println("计算误差")
+	minDiff := 0.0
+	for idx := range o.Output {
+		o.Output[idx].Diff = math.Pow(o.Output[idx].NewValue-o.Output[idx].Value, 2)
+		if minDiff == 0.0 {
+			minDiff = o.Output[idx].Diff
+		}
+		if o.Output[idx].Diff < minDiff {
+			minDiff = o.Output[idx].Diff
+		}
+	}
+	// log.Printf("diff=%.3f", minDiff)
+
+	// 阀值判断
+	if minDiff < o.Diff {
+		return minDiff
+	}
+
+	// 反向计算残差值
+	// log.Println("反向计算残差值")
+	for idx := range o.Output {
+		new, old := o.Output[idx].NewValue, o.Output[idx].Value
+		o.Output[idx].ErrDiff = -(new - old) * new * (1 - new)
+		// log.Printf("errDiff=%.3f", o.Output[idx].ErrDiff)
+	}
+
+	// 计算输入层的误差
+	// 输出 -> 最后一个隐藏层
+	// log.Println("输出 -> 最后一个隐藏层")
+	calcBackward(o.Learn, o.Output, o.Hidden[last])
+
+	// 隐藏 -> 隐藏层
+	// log.Println("隐藏 -> 隐藏层")
+	if last >= 1 {
+		for idx := last; idx > 0; idx-- {
+			calcBackward(o.Learn, o.Hidden[idx], o.Hidden[idx-1])
+		}
+	}
+
+	// 隐藏 -> 输入层
+	// log.Println("隐藏 -> 输入层")
+	calcBackward(o.Learn, o.Hidden[0], o.Input)
+	// }
+
+	return minDiff
 }
 
 // 激活函数
@@ -180,5 +209,6 @@ func randFloat64(min, max float64) float64 {
 	if min == 0 && max == 0 {
 		return 0
 	}
+	// rand.Seed(time.Now().UnixNano())
 	return rand.Float64()*(max-min) + min
 }
--- bpnn_test.go
+++ bpnn_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// go test -timeout 15s -run ^TestNewBPNN$ bpnn -v -count=1
 func TestNewBPNN(t *testing.T) {
 	log.SetFlags(log.Flags() | log.Lshortfile)
 
@@ -20,6 +21,67 @@ func TestNewBPNN(t *testing.T) {
 	nn.Output[0].Weight[0] = 0.2
 	nn.Output[0].Weight[1] = -0.5
 
-	result := nn.Train([]float64{0.4, -0.7}, []float64{0.1})
+	result, _ := nn.Train([][]float64{{0.4, -0.7}}, [][]float64{{0.1}}, 1000)
 	log.Println(result)
+}
+
+// go test -timeout 15s -run ^TestCheck$ bpnn -v -count=1
+func TestCheck(t *testing.T) {
+	log.SetFlags(log.Flags() | log.Lshortfile)
+
+	nn, err := NewBPNN(2, 2, []int{3, 3}, 0.6, 0.0001)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nn.Hidden[0][0].Weight[0] = 0.2
+	nn.Hidden[0][0].Weight[1] = 0.5
+	nn.Hidden[0][1].Weight[0] = 0.3
+	nn.Hidden[0][1].Weight[1] = 0.6
+	nn.Hidden[0][2].Weight[0] = 0.4
+	nn.Hidden[0][2].Weight[1] = 0.7
+	nn.Hidden[1][0].Weight[0] = 0.1
+	nn.Hidden[1][0].Weight[1] = 0.4
+	nn.Hidden[1][0].Weight[2] = 0.7
+	nn.Hidden[1][1].Weight[0] = 0.2
+	nn.Hidden[1][1].Weight[1] = 0.5
+	nn.Hidden[1][1].Weight[2] = 0.8
+	nn.Hidden[1][2].Weight[0] = 0.3
+	nn.Hidden[1][2].Weight[1] = 0.6
+	nn.Hidden[1][2].Weight[2] = 0.9
+	nn.Output[0].Weight[0] = 0.2
+	nn.Output[0].Weight[1] = 0.6
+	nn.Output[0].Weight[2] = 0.3
+	nn.Output[1].Weight[0] = 0.4
+	nn.Output[1].Weight[1] = 0.8
+	nn.Output[1].Weight[2] = 0.5
+
+	result, _ := nn.Train([][]float64{{0.1, 0.2}}, [][]float64{{0.3, 0.4}}, 1000)
+	log.Println(result)
+}
+
+// go test -timeout 15s -run ^Test_1$ bpnn -v -count=1
+func Test_1(t *testing.T) {
+	log.SetFlags(log.Flags() | log.Lshortfile)
+
+	nn, err := NewBPNN(2, 1, []int{3, 3}, 0.6, 0.0001)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	inputData := [][]float64{
+		{0, 0},
+		{0, 1},
+		{1, 0},
+		{1, 1},
+	}
+	outputData := [][]float64{
+		{1},
+		{0},
+		{0},
+		{1},
+	}
+
+	min, count := nn.Train(inputData, outputData, 100000)
+	log.Printf("min:%.8f count:%d\n", min, count)
 }
